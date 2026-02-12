7f091a9afa1b259d094d378efc9cd7fb34825bd2
Michael Durfey <mdurfey@me.com>
2026-02-12 16:04:30 +0300

    fix styling

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,41 @@
-import { Outlet, Link } from "react-router";
+import {
+  Outlet,
+  Link,
+  Meta,
+  Links,
+  ScrollRestoration,
+  Scripts,
+} from "react-router";
+
+export const links = () => [
+  {
+    rel: "stylesheet",
+    href: "/app/tailwind.css",
+  },
+];
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>MPD Photo</title>
+        <meta
+          name="description"
+          content="A modern photo store with React 19, GraphQL and tailwind css"
+        />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
 
 export default function Root(_props: any) {
   return (
